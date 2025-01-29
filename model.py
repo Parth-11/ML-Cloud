3b71d6c31d7d656c96fd6c69eed5a13c16ddf6d2
Parth-11 <parth12.mgg@gmail.com>
2025-01-29 15:40:00 +0530

    Made the Classifier model

---
+++ model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Classifier(nn.Module):
+    def __init__(self,input_size :int):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size,64)
+        self.fc2 = nn.Linear(64,100)
+        self.fc3 = nn.Linear(100,150)
+        self.output = nn.Linear(150,2)
+    
+    def forward(self,x):
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.output(x)
+
+        return F.softmax(x)
