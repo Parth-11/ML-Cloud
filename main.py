358daa187d7e29055e33dc922406e7e2effafc3d
Parth-11 <parth12.mgg@gmail.com>
2025-01-29 15:31:29 +0530

    Changed df data to numpy in main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import data_processing as data
+import pandas as pd
 
-traning_data = data.get_traning_data()
+training_data = data.get_traning_data()
 validation_data = data.get_validation_data()
 
-print(traning_data)
+X_train,y_train = training_data
+X_test, y_test = validation_data
+
+X_train = X_train.to_numpy()
+X_test = X_test.to_numpy()
