60c4efeb1cc26cfee75f1786de1f5b1b0f7ced8d
Parth-11 <parth12.mgg@gmail.com>
2025-02-01 00:05:56 +0530

    Added regression model

--- model.py
+++ model.py
@@ -18,4 +18,19 @@ class Classifier(nn.Module):
         x = F.relu(self.fc3(x))
         x = self.output(x)
 
-        return F.softmax(x)
+        return F.log_softmax(x,dim=1)
+    
+class Regression(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Linear(21,100)
+        self.fc2 = nn.Linear(100,220)
+        self.fc3 = nn.Linear(220,1)
+
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return F.softmax(x,dim=1)
